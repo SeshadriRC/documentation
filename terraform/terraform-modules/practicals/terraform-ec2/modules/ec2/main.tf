3b511287dab1412948cd31495e7dcff9b514fa31
sesha-repo <seshaec1999@gmail.com>
2026-06-14 11:17:13 +0530

    ec2-modules

---
+++ terraform/terraform-modules/practicals/terraform-ec2/modules/ec2/main.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "ec2" {
+
+  ami           = var.ami_id
+  instance_type = var.instance_type
+
+  key_name = var.key_name
+
+  tags = {
+    Name = var.instance_name
+  }
+}
---
+++ terraform/terraform-modules/practicals/terraform-ec2/terraform.tfvars
@@ -0,0 +1,9 @@
+aws_region    = "ap-south-1"
+
+ami_id        = "ami-01a00762f46d584a1"
+
+instance_type = "t2.micro"
+
+instance_name = "cicd"
+
+key_name      = "first-ec2"
